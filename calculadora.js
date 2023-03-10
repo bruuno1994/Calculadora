e5d1664b17f19de1dd7a3f75f12fdd9aa9c84f29
bruuno1994 <bruuno1015@gmail.com>
2023-03-09 21:09:52 -0300

    Ajustes de funções

--- calculadora.js
+++ calculadora.js
@@ -3,38 +3,21 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            visor: '0',
-            caixaTexto: '0',
-            1: 1,
-            2: 2,
-            3: 3,
-            4: 4,
-            5: 5,
-            6: 6,
-            7: 7,
-            8: 8,
-            9: 9,
-            0: 0,
+            resultado: 000,
+            calculo: ""
         }
     },
     methods: {
-        alterarValor: function() {
-            this.resultado = this.caixaTexto;
+        alterarValor(caracter) {
+            this.calculo += caracter
         },
-        soma: function() {
-            this.resultado = parseInt(this.numero1) + parseInt(this.numero2)
+        resultadoCalculo() {
+            this.resultado = eval(this.calculo)
         },
-
-        subtrair: function() {
-            this.resultado = parseInt(this.numero1) - parseInt(this.numero2)
-        },
-
-        multiplicar: function() {
-            this.resultado = parseInt(this.numero1) * parseInt(this.numero2)
-        },
-
-        dividir: function() {
-            this.resultado = parseInt(this.numero1) / parseInt(this.numero2)
+        limparVisor() {
+            this.resultado = 0,
+            this.calculo = 0
+        }
         }
     }
-}).mount("#app");
+).mount("#app");
